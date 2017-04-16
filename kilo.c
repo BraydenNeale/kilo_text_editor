d71836cdbf2e747a6379d21f423919133aa6dfb5
Brayden Neale <brayden.neale@gmail.com>
2017-04-16 13:30:27 -0400

    Add a read timeout

--- kilo.c
+++ kilo.c
@@ -21,6 +21,9 @@ void enableRawMode()
 	raw.c_iflag &= ~(OPOST);
 	raw.c_iflag |= (CS8);
   	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
   	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 	
@@ -28,9 +31,10 @@ int main()
 {
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	while (1)
 	{
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if (iscntrl(c))
 		{
 			printf("%d\r\n", c);
@@ -39,6 +43,7 @@ int main()
 		{
 			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 	return 0;
 }
