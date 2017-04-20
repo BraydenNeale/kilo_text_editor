defe66847a4ffcb67101c5c5149d05075582eadf
Brayden Neale <brayden.neale@gmail.com>
2017-04-19 21:42:00 -0400

    Fixed a bug that was hiding the cursor icon

--- kilo.c
+++ kilo.c
@@ -213,7 +213,6 @@ void editorRefreshScreen()
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
 	abAppend(&ab, buf, strlen(buf));
 
-	abAppend(&ab, "\x1b[H", 3);
 	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
