b961eb64dfbf73484444bf2dcabf1829ae64695b
Brayden Neale <brayden.neale@gmail.com>
2017-04-30 16:48:21 -0400

    Keep track of whether a file has been modified

--- kilo.c
+++ kilo.c
@@ -53,6 +53,7 @@ struct editorConfig {
 	int screencols;
 	int numrows;
 	erow *row;
+	int dirty;
 	char *filename;
 	char statusmsg[80];
 	time_t statusmsg_time;
@@ -265,6 +266,7 @@ void editorAppendRow(char *s, size_t len)
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
+	E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c)
@@ -275,6 +277,7 @@ void editorRowInsertChar(erow *row, int at, int c)
 	row->size++;
 	row->chars[at] = c;
 	editorUpdateRow(row);
+	E.dirty++;
 }
 
 /*** editor operations ***/
@@ -336,6 +339,7 @@ void editorOpen(char * filename)
 	}
 	free(line);
 	fclose(fp);
+	E.dirty = 0;
 }
 
 void editorSave()
@@ -354,6 +358,7 @@ void editorSave()
 			{
 				close(fd);
 				free(buf);
+				E.dirty = 0;
 				editorSetStatusMessage("%d bytes written to disk", len);
 				return;
 			}
@@ -464,8 +469,9 @@ void editorDrawStatusBar(struct abuf *ab)
 	char status[80];
 	char rstatus[80];
 	
-	int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-			E.filename ? E.filename : "[No Name]", E.numrows);
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+			E.filename ? E.filename : "[No Name]", E.numrows,
+			E.dirty ? "(modified)" : "");
 	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
 			E.cy + 1, E.numrows);
 
@@ -668,6 +674,7 @@ void initEditor()
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.dirty = 0;
 	E.filename = NULL;
 	E.statusmsg[0] = '\0';
 	E.statusmsg_time = 0;
